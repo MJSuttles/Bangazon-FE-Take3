c67b74663dbdce70ea1b9f1428f86bc51006f407
MJSuttles <rolltiderolldad@gmail.com>
2025-03-08 12:59:25 -0600

    created initial folder structure

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,59 @@
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+
+// const signIn = () => {
+//   const provider = new firebase.auth.GoogleAuthProvider();
+//   firebase.auth().signInWithPopup(provider);
+// };
+
+// const signOut = () => {
+//   firebase.auth().signOut();
+// };
+
+// export { signIn, signOut };
+
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { clientCredentials } from './client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const checkUser = (uid) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/checkuser/${uid}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(async (res) => {
+        let data;
+        console.log('status:', res);
+        if (res.status === 204) {
+          resolve({});
+        } else {
+          data = await res.json();
+          // console.log('data:', data);
+          resolve(data);
+        }
+      })
+      .catch(reject);
+  });
+
+const registerUser = (userInfo) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/users/`, {
+      method: 'POST',
+      body: JSON.stringify(userInfo),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((resp) => resolve(resp.json()))
+      .catch(reject);
+  });
 
 const signIn = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -10,4 +64,9 @@ const signOut = () => {
   firebase.auth().signOut();
 };
 
-export { signIn, signOut };
+export {
+  signIn, //
+  signOut,
+  checkUser,
+  registerUser,
+};
