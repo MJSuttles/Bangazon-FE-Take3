c4409dac44c35bb50a484430c8cb2bb0d5440349
MJSuttles <rolltiderolldad@gmail.com>
2025-03-11 21:50:50 -0500

    added User Profile card

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,17 +1,3 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-
-// const signIn = () => {
-//   const provider = new firebase.auth.GoogleAuthProvider();
-//   firebase.auth().signInWithPopup(provider);
-// };
-
-// const signOut = () => {
-//   firebase.auth().signOut();
-// };
-
-// export { signIn, signOut };
-
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { clientCredentials } from './client';
